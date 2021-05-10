2c475560ec064abcb8bbb81aa045046d44b8ee86
Катя Нетребич <knetrebich@gmail.com>
2021-05-10 21:13:46 +0300

    create CSV file

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace CSVFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSVConvert csvConvert = new CSVConvert(PersonList.GetListPerson());
+            string path = @"C:\Users\Kate\Desktop";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            using (FileStream fstream = new FileStream($@"{path}\CSVFile.csv", FileMode.OpenOrCreate))
+            {
+                byte[] array = System.Text.Encoding.Default.GetBytes(csvConvert.Export());
+                fstream.Write(array, 0, array.Length);
+                Console.WriteLine("Текст записан в файл");
+            }
+        }
+    }
+}
