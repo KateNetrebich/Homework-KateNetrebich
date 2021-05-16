8ac29ac0833a03a52998f3d8fab5df8dff4f06c7
Катя Нетребич <knetrebich@gmail.com>
2021-05-16 23:38:01 +0300

    Linq homework part 2

---
+++ Actor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqHomeworkPart2
+{
+    class Actor
+    {
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
---
+++ ArtObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqHomeworkPart2
+{
+    abstract class ArtObject
+    {
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+}
