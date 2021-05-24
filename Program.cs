b9a0d43ff2ec703666d323f530cf4146cf2a5287
Катя Нетребич <knetrebich@gmail.com>
2021-05-24 23:21:15 +0300

    Thread homework

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace Threadings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            decimal[] array = new decimal[10];
+            Thread randomArray = new Thread(()=>
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = random.Next(1,10);
+                }
+                foreach (var a in array)
+                {
+                    Console.Write(a);
+                }
+                Console.WriteLine("\n");
+            });
+
+            Thread arrayPart = new Thread(() => 
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = random.Next(1, 10);
+                }
+                decimal[] arr = new decimal[array.Length/2];
+                arr = array;
+                foreach (var a in arr)
+                {
+                    Console.Write(a);
+                }
+                Console.WriteLine("\n");
+            });
+
+            Thread arrayMin = new Thread(()=> 
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = random.Next(1, 10);
+                }
+                Console.Write($"{array.Min()}\n");
+            });
+
+            Thread arrayAvarage = new Thread(()=> 
+            {
+                decimal sum = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = random.Next(1, 10);
+                    sum += array[i];
+                }
+                Console.Write($"{sum/array.Length}\n");
+            });
+
+            randomArray.Start();
+            arrayPart.Start();
+            arrayMin.Start();
+            arrayAvarage.Start();
+
+            randomArray.Join();
+            arrayPart.Join();
+            arrayMin.Join();
+            arrayAvarage.Join();
+        }
+    }
+}
